2078a8e1c0abee00ba5864d80fdef9eb2315aa65
Simon Busch <simon.busch@live.fr>
2020-11-18 11:30:07 +0100

    routing done / reviews done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get '/restaurants', to: "restaurant#index"
+
+  # get '/restaurants/new', to: "restaurant#new", as: :new_restaurant
+  # post '/restaurants', to: "restaurant#create"
+
+  # get '/restaurant/:id', to: "restaurant#show", as: :restaurant
+
+  # get '/restaurant/:id/review/new', to: "review#new", as: :new_review
+  # post '/restaurant/:id/review', to: "review#create"
+
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 end
